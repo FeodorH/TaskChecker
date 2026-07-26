9c1f83a63926880c554d7ebc6b1bcb95775a2067
FeodorH <fholkin@yandex.ru>
2026-07-26 16:26:39 +0300

    add postman specs

--- src/main/kotlin/com/example/myFirstSpringProject/Model.kt
+++ src/main/kotlin/com/example/myFirstSpringProject/Model.kt
@@ -1,5 +1,6 @@
 package com.example.myFirstSpringProject
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
